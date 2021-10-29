e04ad9023c3af16749f327625cd58a800628b3e4
maxrem <max.remkes@gmail.com>
2021-10-29 14:28:56 +0200

    todo

--- src/helpers.js
+++ src/helpers.js
@@ -1,6 +1,6 @@
 export const Datetime = {
     format(datetimeString) {
         let datetime = new Date(datetimeString);//Date.parse(datetimeString);
-        return datetime.toLocaleString('nl-NL', { timeZone: 'Europe/Amsterdam' }); // should be UTC
+        return datetime.toLocaleString('nl-NL', { timeZone: 'Europe/Amsterdam' }); // TODO should be UTC, or move to config
     }
 }
